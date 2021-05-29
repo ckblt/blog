13b38ecacbafe736baac994267b01ad7930d8ca3
ckblt <964463815@qq.com>
2021-05-29 09:22:41 +0800

    config

---
+++ config/config.default.js
@@ -0,0 +1,54 @@
+const readFileSync = require("fs").readFileSync;
+
+/**
+ * @param {import("egg").EggAppInfo} appInfo
+ */
+module.exports = (appInfo) => {
+  /**
+   * @type {import("egg").EggAppConfig}
+   */
+  let config = {};
+
+  config.keys = appInfo.name + "_12";
+
+  // 网站 icon
+  config.siteFile = {
+    "/favicon.ico": readFileSync(
+      __dirname + "/../app/public/favicon/favicon.png"
+    ),
+  };
+
+  //
+  config.notfound = {
+    pageUrl: "/static/html/404.html",
+  };
+
+  config.view = {
+    defaultViewEngine: "nunjucks",
+    mapping: {
+      ".html": "nunjucks",
+    },
+  };
+
+  config.static = {
+    prefix: "/static/",
+  };
+
+  config.mysql = {
+    client: {
+      // xxxxx
+    },
+    app: true,
+    agent: false,
+  };
+
+  config.cluster = {
+    listen: {
+      path: "",
+      port: 7001,
+      hostname: "192.168.1.3",
+    },
+  };
+
+  return config;
+};
